e65f344c0ae2172a4603624c3a907a37bf9d30e7
妖风伴残月 <43426976+mathLjtao@users.noreply.github.com>
2019-08-19 22:00:48 +0800

    fam

--- src/main/java/com/ljtao3/service/SysUserService.java
+++ src/main/java/com/ljtao3/service/SysUserService.java
@@ -3,19 +3,29 @@ package com.ljtao3.service;
 import com.ljtao3.dao.SysUserMapper;
 import com.ljtao3.model.SysUser;
 import com.ljtao3.param.UserParam;
+import com.ljtao3.util.PasswordUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 @Service
 public class SysUserService {
     @Resource
     private SysUserMapper sysUserMapper;
     public void save(UserParam param){
-        String password="123456";
-        SysUser sysUser=SysUser.builder().username(param.getUsername()).telephone(param.getTelephone()).mail(param.getMail()).deptId(param.getDeptId())
+        Integer id = param.getId();
+        checkEmailExist(param.getMail(),id);
+        checkTelephoneExist(param.getTelephone(),id);
+        String password= PasswordUtil.randomPassword();
+        password="123456";//todo
+        SysUser user=SysUser.builder().username(param.getUsername()).telephone(param.getTelephone()).mail(param.getMail()).deptId(param.getDeptId())
                 .password(password).status(param.getStatus()).remark(param.getRemark()).build();
-
+        user.setOperator("system");
+        user.setOperateIp("127.0.0.1");
+        user.setOperateTime(new Date());
+        //TODO : sendEmail
+        sysUserMapper.insertSelective(user);
     }
     public boolean checkEmailExist(String mail,Integer userId){
         return false;
